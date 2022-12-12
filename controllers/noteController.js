14961e580e1ee607d27e006ba291f66fd4f81363
abetancurtb <andres.betancurt.mt@usa.edu.co>
2022-12-11 19:29:43 -0500

    merge

---
+++ controllers/noteController.js
@@ -0,0 +1,13 @@
+export function createNote(req, res){
+    
+    res.sendStatus(200)
+}
+export function readNote(req, res){
+    res.sendStatus(200)
+}
+export function updateNote(req, res){
+    res.sendStatus(200)
+}
+export function deleteNote(req, res){
+    res.sendStatus(200)
+}
