a1b9928ef81e0bd7640115a95c43ce2804531327
abetancurtb <andres.betancurt.mt@usa.edu.co>
2022-12-20 21:36:42 -0500

    sprint2

---
+++ controllers/noteController.js
@@ -0,0 +1,74 @@
+import noteModel from  "../models/note.js"
+
+
+export async function createNote(req, res){
+    const note = req.body;
+
+    let documento
+
+    try {
+        documento = await noteModel.create(note)
+    } catch (error) {
+        res.status(400)
+        res.json(error.message)
+        return;
+    }
+
+    res.status(201)
+    res.json(documento)
+}
+
+export async function readNote(req, res){
+    const { idUsuario } = req.body
+
+    let documento
+
+    try {
+        documento = await noteModel.findOne({ idUsuario })
+    } catch (error) {
+        res.status(400)
+        res.json(error.message)
+        return;
+    }
+
+    res.status(200)
+    res.json(documento)
+}
+
+export async function updateNote(req, res){
+    const { idUsuario } = req.params
+    const { cambios } = req.body
+
+    let documento;
+
+    try {
+        documento = await noteModel.updateOne({
+            idUsuario
+        }, cambios)
+    } catch (error) {
+        res.status(400)
+        res.json(error.message)
+        return;
+    }
+
+    res.status(200)
+    res.json(documento)
+}
+export async function deleteNote(req, res){
+    const { id } = req.params
+
+    let documento;
+
+    try {
+        documento = await noteModel.findOneAndDelete({
+            "_id": id
+        })
+    } catch (error) {
+        res.status(400)
+        res.json(error.message)
+        return;
+    }
+
+    res.status(200)
+    res.json(documento)
+}
